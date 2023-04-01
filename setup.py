b5cc749c209a04c596fff67dfc28177b6a1ef016
daricht <80166694+daricht@users.noreply.github.com>
2023-04-01 15:26:02 +0200

    updated setup.py names etc.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "ZZYYC"
+plugin_identifier = "Probing"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_ZZYYC"
+plugin_package = "octoprint_Probing"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Zzyyc"
+plugin_name = "Surface Probing"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.1"
