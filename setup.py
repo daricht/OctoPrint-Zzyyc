6ff2df5a04a8044d1028271fbc7b606293e3d007
daricht <80166694+daricht@users.noreply.github.com>
2023-04-01 15:27:50 +0200

    Revert "updated setup.py names etc."
    This reverts commit b5cc749c209a04c596fff67dfc28177b6a1ef016.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "Probing"
+plugin_identifier = "ZZYYC"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_Probing"
+plugin_package = "octoprint_ZZYYC"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Surface Probing"
+plugin_name = "OctoPrint-Zzyyc"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.1"
